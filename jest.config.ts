d95d7fecf223c61efc96f9a6efccf32ecf83dde5
samdouble <samleafs@protonmail.com>
2026-07-01 10:47:07 -0400

    fix: upgrade node types

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,12 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // chai-as-promised v8+ and check-error are ESM-only
+  transformIgnorePatterns: [
+    'node_modules/(?!(chai-as-promised|check-error)/)',
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    'node_modules/(chai-as-promised|check-error)/.+\\.js$': 'babel-jest',
+  },
 };
